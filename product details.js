ed278381ef52e7c2e0e3bd15a68240daeb4d482f
Hardik769459 <kuntehardik@gmail.com>
2025-10-16 22:46:24 +0530

    Update product details.js

---
+++ product details.js
@@ -0,0 +1,25 @@
+const products = [
+  {
+    id: '1',
+    name: 'iPhone 15 Pro',
+    description: '6.1-inch, A17 Pro chip, Triple-camera, 5G',
+    price: 129999,
+    image: 'https://store.storeimages.cdn-apple.com/iphone-15-pro.png'
+  },
+  {
+    id: '2',
+    name: 'Samsung Galaxy S24 Ultra',
+    description: '6.8-inch AMOLED, Snapdragon 8 Gen 3, Quad camera',
+    price: 109999,
+    image: 'https://images.samsung.com/galaxy-s24-ultra.png'
+  },
+  {
+    id: '3',
+    name: 'OnePlus 12',
+    description: '6.7-inch AMOLED, Snapdragon 8 Gen 3, Hasselblad camera',
+    price: 74999,
+    image: 'https://oneplus.com/oneplus-12.png'
+  }
+];
+
+export default products;
